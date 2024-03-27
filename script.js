da087b7f7ee6d0ee76babbe6a9a38720635f6898
MERLIN752 <74856757+MERLIN752@users.noreply.github.com>
2024-03-27 14:36:17 +0100

    Corrections des questions / réponses

--- script.js
+++ script.js
@@ -7,47 +7,48 @@ document.addEventListener("DOMContentLoaded", function() {
       {
           question: "Bonjour ! Comment puis-je vous aider ?",
           answers: [
-              { text: "quel est le language de programmation le plus utilisé ?", nextQuestion: 1 },
-              { text: "ou trouvé des documentation ?", nextQuestion: 2 },
-              { text: "ou publier des programme crée ?", nextQuestion: 3 },
-              { text: "ou installer go ?", nextQuestion: 4 },
-              { text: "quel est le premier language de programation ?", nextQuestion: 5 },
-              { text: "comment se former à la programmation ?", nextQuestion: 6 }
+              { text: "Quel est le language de programmation le plus utilisé ?", nextQuestion: 1 },
+              { text: "Ou peut-on trouver des documentations pour coder ?", nextQuestion: 2 },
+              { text: "Ou publier des programme créés ?", nextQuestion: 3 },
+              { text: "Ou installer go ?", nextQuestion: 4 },
+              { text: "Quel est le premier language de programation ?", nextQuestion: 5 },
+              { text: "Comment se former à la programmation ?", nextQuestion: 6 }
 
           ]
       },
       {
-          question: "pour des jeux video python, C++ pour des site web js, HTML/CSS",
+          question: "Python est globalement le plus utilisé. Il est principalement utilisé en Data. Pour des jeux video, les plus utilisés sont C++ et C#." +
+              " Pour des site web, il s'agit d'HTML/CSS, JavaScript et PHP",
           answers: [
             {text: "done", nextQuestion: null}
           ]
       },
       {
-          question: "sur devdocs vous pourrez trouver la documentation de plusieur langage  https://devdocs.io/",
+          question: "Sur devdocs vous pourrez trouver la documentation de plusieurs langages de programmation : https://devdocs.io/",
           answers: [
             {text: "done", nextQuestion: null}
         ]
       },
       {
-          question: "sur github il permet aussi de travailler en groupe plus facilement https://github.com/",
+          question: "Sur github : Il permet également de travailler en groupe plus facilement https://github.com/",
           answers: [
             {text: "done", nextQuestion: null}
         ]
       },
       {
-          question: "sur le site de docs officiel de go https://go.dev/doc/install",
+          question: "Vous pouvez le faire sur le site de docs officiel de go https://go.dev/doc/install",
           answers: [
             {text: "done", nextQuestion: null}
         ]
       },
       {
-          question: "Prolog en 1972 par Colmerauer",
+          question: "Il s'agit de Prolog en 1972 par Colmerauer",
           answers: [
             {text: "done", nextQuestion: null}
         ]
       },
       {
-          question: "openClasseroom peut fournir des cour sur pluisieur language de programation https://openclassrooms.com/",
+          question: "OpenClasseroom peut fournir des cours sur plusieurs langages de programmation : https://openclassrooms.com/",
           answers: [
             {text: "done", nextQuestion: null}
         ]
