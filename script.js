bcfa69ae51789636f5f2044df04608f112e15562
peanthony <anthony.pereira@ynov.com>
2024-03-27 13:55:18 +0100

    added the question

--- script.js
+++ script.js
@@ -7,23 +7,50 @@ document.addEventListener("DOMContentLoaded", function() {
       {
           question: "Bonjour ! Comment puis-je vous aider ?",
           answers: [
-              { text: "Comment ça va ?", nextQuestion: 1 },
-              { text: "Quel est ton nom ?", nextQuestion: 2 },
-              { text: "Quel est ton nom ?", nextQuestion: 3 }
+              { text: "quel est le language de programmation le plus utilisé ?", nextQuestion: 1 },
+              { text: "ou trouvé des documentation ?", nextQuestion: 2 },
+              { text: "ou publier des programme crée ?", nextQuestion: 3 },
+              { text: "ou installer go ?", nextQuestion: 4 },
+              { text: "quel est le premier language de programation ?", nextQuestion: 5 },
+              { text: "comment se former à la programmation ?", nextQuestion: 6 }
 
           ]
       },
       {
-          question: "Je suis un robot, donc ça va toujours bien ! Et vous ?",
+          question: "pour des jeux video python, C++ pour des site web js, HTML/CSS",
           answers: [
-              { text: "Bien, merci.", nextQuestion: null }
+            {text: "done", nextQuestion: null}
           ]
       },
       {
-          question: "Je suis un chatbot. Vous pouvez m'appeler Chatbot.",
+          question: "sur devdocs vous pourrez trouver la documentation de plusieur langage  https://devdocs.io/",
           answers: [
-              { text: "D'accord, merci.", nextQuestion: null }
-          ]
+            {text: "done", nextQuestion: null}
+        ]
+      },
+      {
+          question: "sur github il permet aussi de travailler en groupe plus facilement https://github.com/",
+          answers: [
+            {text: "done", nextQuestion: null}
+        ]
+      },
+      {
+          question: "sur le site de docs officiel de go https://go.dev/doc/install",
+          answers: [
+            {text: "done", nextQuestion: null}
+        ]
+      },
+      {
+          question: "Prolog en 1972 par Colmerauer",
+          answers: [
+            {text: "done", nextQuestion: null}
+        ]
+      },
+      {
+          question: "openClasseroom peut fournir des cour sur pluisieur language de programation https://openclassrooms.com/",
+          answers: [
+            {text: "done", nextQuestion: null}
+        ]
       }
   ];
 
@@ -41,7 +68,8 @@ document.addEventListener("DOMContentLoaded", function() {
               if (answer.nextQuestion !== null) {
                   displayQuestion(answer.nextQuestion);
               } else {
-                  appendMessage("user", answer.text);
+                displayQuestion(0);
+                const currentQuestion = options[0];
               }
           });
           chatArea.appendChild(answerButton);
